dc21de7ca57962aa51403da7d292eb0c6238206b
berkaykurkcu <berkay.kurkcuyt@gmail.com>
2022-11-05 22:46:29 +0300

    vücut kitle indeksi hesaplayan

--- javaPatikasi/src/homework1.java
+++ javaPatikasi/src/homework1.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class homework1 {
+import java.util.Scanner;
+
+public class homework1 {
+    public static void main(String[] args) {
+        double boy,kilo,total;
+        System.out.println("Vücut kitle endeksi hesaplayan program");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Boyunuzu giriniz:");
+        boy = scan.nextDouble();
+        System.out.println("Kilonuzu giriniz:");
+        kilo = scan.nextDouble();
+        total = kilo / (boy*boy);
+        System.out.println(total);
+    }
 }
