e5d0f203762dc2f217c84776957aef539f0c3938
berkaykurkcu <berkay.kurkcuyt@gmail.com>
2022-11-05 22:34:51 +0300

    hipotenüs bulma java files

--- javaPatikasi/src/practice3.java
+++ javaPatikasi/src/practice3.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class practice3 {
+import java.util.Scanner;
+public class practice3 {
+    public static void main(String[] args) {
+        System.out.println("Hipotenüs hesaplama");
+        int a,b;
+        double c;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("A kenarını giriniz: ");
+        a = scan.nextInt();
+        System.out.println("B kenarını giriniz: ");
+        b = scan.nextInt();
+        c = a*a + b*b;
+        c = Math.sqrt(c);
+        System.out.println("Hipotenüs: " + c );
+    }
 }
