23d9c1f454f4c294193d0a70a5443322318e3a7e
berkaykurkcu <berkay.kurkcuyt@gmail.com>
2022-11-05 22:20:32 +0300

    renames

--- javaPatikasi/src/practice2.java
+++ javaPatikasi/src/practice2.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class practice2 {
+import java.util.Scanner;
+public class practice2 {
+    public static void main(String[] args) {
+        System.out.println("Gelirinizi giriniz! Çıkmak için -1");
+        int gelir = 0;
+        Scanner scan = new Scanner(System.in);
+        while(gelir != -1){
+            gelir = scan.nextInt();
+            int sonuc = gelir>=1000 ? gelir * 8/100 + gelir : gelir * 18/100 + gelir;
+            System.out.println(gelir == -1 ? "Çıkış yapılıyor." : "KDV: " + sonuc);
+        }
+    }
 }
