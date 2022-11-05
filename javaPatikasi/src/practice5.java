93f67adbea634b04119fc8c3875aa5adf62777cf
berkaykurkcu <berkay.kurkcuyt@gmail.com>
2022-11-05 22:43:29 +0300

    dairenin alanı ve yarııçapını hesapla

--- javaPatikasi/src/practice5.java
+++ javaPatikasi/src/practice5.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class practice5 {
+import java.util.Scanner;
+
+public class practice5 {
+    public static void main(String[] args) {
+        int r;
+        double pi = 3.14;
+        System.out.println("Dairenin alanını ve çevresini bulan program");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Yarıçapı gir:");
+        r = scan.nextInt();
+        double alan = pi * r * r;
+        double cevre = 2 * pi * r;
+        System.out.println("Alan:" + alan + " Çevre:" + cevre);
+    }
 }
