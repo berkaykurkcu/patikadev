f04b4d813fad26d52fb8ddbd13b28d808e6daa66
berkaykurkcu <berkay.kurkcuyt@gmail.com>
2022-11-05 22:40:18 +0300

    taksimetre hesaplama

--- javaPatikasi/src/practice4.java
+++ javaPatikasi/src/practice4.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class practice4 {
+import java.util.Scanner;
+public class practice4 {
+    public static void main(String[] args) {
+        System.out.println("Taksiye hoşgeldiniz");
+        double acilisUcreti = 10,kmBasinUcret = 2.20,totalUcret,km;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Total KM giriniz:");
+        km = scan.nextDouble();
+        totalUcret = acilisUcreti + kmBasinUcret * km;
+        if(totalUcret<=20.0){
+            System.out.println("Ücretiniz: " +totalUcret +"\n Minimum ücret olan 20TL 'yi ödemelisiniz.");
+        }else{
+            System.out.println("Ücretiniz:" + totalUcret);
+        }
+    }
 }
