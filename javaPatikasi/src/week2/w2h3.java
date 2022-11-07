f913506f567c9804498abb3eb9489b515b57384c
berkaykurkcu <berkay.kurkcuyt@gmail.com>
2022-11-07 16:07:29 +0300

    leap year

--- javaPatikasi/src/week2/w2h3.java
+++ javaPatikasi/src/week2/w2h3.java
@@ -1,2 +1,20 @@
-package week2;public class w2h3 {
+package week2;
+
+import java.util.Scanner;
+
+public class w2h3 {
+    public static void main(String[] args) {
+        System.out.println("Week 2 Homework 3");
+        System.out.println("Yıl giriniz");
+        int yil;
+        Scanner scan = new Scanner(System.in);
+        yil = scan.nextInt();
+        if ((yil % 4 == 0) && (yil % 100 != 0)) {
+            System.out.println(yil + " is a leap year.");
+        } else if ((yil % 100 == 0) && (yil % 400 == 0)) {
+            System.out.println(yil + " is a leap year.");
+        } else {
+            System.out.println(yil + " is not a leap year.");
+        }
+    }
 }
