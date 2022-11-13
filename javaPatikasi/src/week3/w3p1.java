5b0a34ebfa40e6a1b09bdaea6643ffd121977923
berkaykurkcu <berkay.kurkcuyt@gmail.com>
2022-11-13 14:44:55 +0300

    w3p1

--- javaPatikasi/src/week3/w3p1.java
+++ javaPatikasi/src/week3/w3p1.java
@@ -1,7 +1,21 @@
 package week3;
 
+import java.util.Scanner;
+
 public class w3p1 {
     public static void main(String[] args) {
         System.out.println("Find even numbers up until the input number");
+	int k;
+	Scanner inp = new Scanner(System.in);
+
+	System.out.println("Sayı giriniz: ");
+	k = inp.nextInt();
+
+	if(k % 2 == 0){
+		System.out.println("K is even");
+	}else{
+		System.out.println("K is odd");
     }
 }
+}
+
