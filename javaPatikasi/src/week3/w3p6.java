0047c69581f96630ee56decc3b7ca2db728cffac
berkaykurkcu <berkay.kurkcuyt@gmail.com>
2022-11-15 09:24:37 +0300

    armstrong numbers

--- javaPatikasi/src/week3/w3p6.java
+++ javaPatikasi/src/week3/w3p6.java
@@ -1,7 +1,30 @@
 package week3;
 
+import java.util.Scanner;
+
 public class w3p6 {
     public static void main(String[] args) {
-        System.out.println();
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Lütfen sayıyı giriniz:" );
+        int number = input.nextInt();
+        int tempnumber = number;
+        int digit = 0;
+        int value, a = 0;
+
+        // a = sonuç, digit = basamak sayısı
+
+        while (tempnumber != 0) {
+            tempnumber /= 10;
+            digit++;
+        }
+
+        tempnumber = number;
+        while (tempnumber != 0) {
+            value = tempnumber % 10;
+            a += value;
+            tempnumber /= 10;
+        }
+        System.out.println(a);
     }
 }
