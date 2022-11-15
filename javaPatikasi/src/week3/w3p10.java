d8aa3c5c8cb94bbdd44d540625bbbc1086258223
berkaykurkcu <berkay.kurkcuyt@gmail.com>
2022-11-15 17:02:21 +0300

    w3p10 implemented

--- javaPatikasi/src/week3/w3p10.java
+++ javaPatikasi/src/week3/w3p10.java
@@ -1,2 +1,18 @@
-package week3;public class w3p10 {
+package week3;
+
+public class w3p10 {
+    public static void main(String[] args) {
+        System.out.println("W3P10");
+        int sayi1 = 18, sayi2=24;
+        int ebob=1,ekok;
+        int j = 1;
+        while(j<=sayi1 && j<=sayi2){
+            if(sayi1 % j == 0 && sayi2  % j == 0){
+                System.out.println("While ile bulduk!" + j);
+                ebob = j;
+            }j++;
+        }
+        ekok = (sayi1*sayi2)/ebob;
+        System.out.println("Ebob: " + ebob + " Ekok: " + ekok);
+    }
 }
