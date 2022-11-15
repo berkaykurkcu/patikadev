0c5e0dd20246d7594848ba53aef2f064f71bba96
berkaykurkcu <berkay.kurkcuyt@gmail.com>
2022-11-15 17:27:01 +0300

    w3h1 complete

--- javaPatikasi/src/week3/w3h1.java
+++ javaPatikasi/src/week3/w3h1.java
@@ -1,2 +1,23 @@
-package week3;public class w3h1 {
+package week3;
+
+import java.util.Scanner;
+
+public class w3h1 {
+    public static void main(String[] args) {
+        System.out.println("Perfect number");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number to see if it is perfect or not: ");
+        int number = scan.nextInt();
+        int total=0;
+        for(int i = 1;i<number;i++){
+            if(number%i==0){
+                total += i;
+            }
+        }
+        if(number==total){
+            System.out.println("Perfect!!");
+        }else{
+            System.out.println("Not perfect!");
+        }
+    }
 }
