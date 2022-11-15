204b7558088ccc192de2c4557f798206beb45df6
berkaykurkcu <berkay.kurkcuyt@gmail.com>
2022-11-15 09:20:13 +0300

    w3p5-p6

--- javaPatikasi/src/week3/w3p5.java
+++ javaPatikasi/src/week3/w3p5.java
@@ -1,2 +1,22 @@
-package week3;public class w3p5 {
+package week3;
+
+import java.util.Scanner;
+
+public class w3p5 {
+    public static void main(String[] args) {
+        System.out.println("Calculate power");
+        int k,n;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter bottom number");
+        k = scan.nextInt();
+        System.out.println("Enter power");
+        n  = scan.nextInt();
+        int result = (int) Math.pow(k,n);
+        int result2=1;
+        System.out.println("Result is: " + result);
+        for(int i = 0; i < n; i++){
+            result2 = result2 * k;
+        }
+        System.out.println("Result2: " +result2);
+    }
 }
--- javaPatikasi/src/week3/w3p6.java
+++ javaPatikasi/src/week3/w3p6.java
@@ -1,2 +1,7 @@
-package week3;public class w3p6 {
+package week3;
+
+public class w3p6 {
+    public static void main(String[] args) {
+        System.out.println();
+    }
 }
