129b932a3b67f0efbb646fe58bbe81bb5ddb742d
berkaykurkcu <berkay.kurkcuyt@gmail.com>
2022-11-17 15:48:49 +0300

    w3h4 implemented

--- javaPatikasi/src/week3/w3h4.java
+++ javaPatikasi/src/week3/w3h4.java
@@ -1,7 +1,22 @@
 package week3;
 
+import java.util.Scanner;
+
 public class w3h4 {
     public static void main(String[] args) {
-        System.out.println("");
+        System.out.println("Asal sayıları yazdır");
+        int counter = 0;
+        for(int i = 2; i <= 100; i++){
+            for(int j = 2; j < i; j++){
+                if(i % j == 0){
+                    counter++;
+                    break;
+                }
+            }
+            if(counter == 0){
+                System.out.println(i + " ");
+            }
+            counter = 0;
+        }
     }
 }
