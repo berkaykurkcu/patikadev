0ddd639a267346c8569ffe96507269e1cbc48836
berkaykurkcu <berkay.kurkcuyt@gmail.com>
2022-11-17 15:42:53 +0300

    renamed

--- javaPatikasi/src/week3/w3h1.java
+++ javaPatikasi/src/week3/w3h1.java
@@ -1,23 +1,41 @@
 package week3;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
-public class w3h1 {
+public class w3p11 {
     public static void main(String[] args) {
-        System.out.println("Perfect number");
+        System.out.println("N adet sayma sayısının en küçüğünü en büyüğünü bulma");
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter a number to see if it is perfect or not: ");
-        int number = scan.nextInt();
-        int total=0;
-        for(int i = 1;i<number;i++){
-            if(number%i==0){
-                total += i;
+        int biggest=Integer.MIN_VALUE;
+        int smallest=Integer.MAX_VALUE;
+
+        System.out.println("Kaç tane sayı gireceksiniz?");
+        int count = scan.nextInt();
+        int[] liste = new int[count];
+        for(int i = 0;i<count;i++){
+            System.out.println((i+1) +". sayıyı giriniz");
+            liste[i] = scan.nextInt();
+        }
+        System.out.println(Arrays.toString(liste));
+        for(int j=0; j<liste.length;j++){
+            for(int k=0; k<liste.length;k++){
+                if(liste[j] < liste[k]){
+                    System.out.println(liste[j] + " is smaller than " + liste[k]);
+                    if(smallest>liste[j]){
+                        smallest = liste[j];
+                    }
+                }else if(liste[j] == liste[k]){
+                    System.out.println(liste[j] + " is equal to " +liste[k]);
+                }else{
+                System.out.println(liste[j] + " is bigger than " + liste[k]);
+                biggest = liste[j];
+                if(biggest<liste[j]){
+                    biggest = liste[j];
+                }
+                }
             }
         }
-        if(number==total){
-            System.out.println("Perfect!!");
-        }else{
-            System.out.println("Not perfect!");
-        }
+        System.out.println("Biggest: " + biggest + " smallest: " + smallest);
     }
 }
---
+++ javaPatikasi/src/week3/w3h3.java
@@ -0,0 +1,11 @@
+package week3;
+
+import java.util.Scanner;
+
+public class w3h2 {
+    public static void main(String[] args) {
+        System.out.println("Reverse triangle, enter row amount");
+        Scanner scan = new Scanner(System.in);
+        int row = scan.nextInt();
+    }
+}
