a906238bb4ab6626d070ac999351537d8110545d
berkaykurkcu <berkay.kurkcuyt@gmail.com>
2022-11-17 15:45:02 +0300

    w3h3 4

--- javaPatikasi/src/week3/w3h3.java
+++ javaPatikasi/src/week3/w3h3.java
@@ -2,10 +2,24 @@ package week3;
 
 import java.util.Scanner;
 
+
 public class w3h3 {
     public static void main(String[] args) {
-        System.out.println("Reverse triangle, enter row amount");
-        Scanner scan = new Scanner(System.in);
-        int row = scan.nextInt();
+        int n = 0;
+        Scanner inp = new Scanner(System.in);
+        do {
+            System.out.println("Please enter a number:");
+            n = inp.nextInt();
+        }while (n < 0);
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1 ; j <= i - 1 ; j++) {
+                System.out.print(" ");
+            }
+            for(int k = (2 * n) - 1; k >= (2*i) - 1 ; k--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
--- javaPatikasi/src/week3/w3h4.java
+++ javaPatikasi/src/week3/w3h4.java
@@ -1,2 +1,7 @@
-package week3;public class w3h4 {
+package week3;
+
+public class w3h4 {
+    public static void main(String[] args) {
+        System.out.println("");
+    }
 }
