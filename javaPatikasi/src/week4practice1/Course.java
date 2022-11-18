e4fad9ffe1360bee3f93f2f27b73a1461aac4915
berkaykurkcu <berkay.kurkcuyt@gmail.com>
2022-11-18 18:43:37 +0300

    w4p1

---
+++ javaPatikasi/src/week4practice1/Course.java
@@ -0,0 +1,2 @@
+package week4practice1;public class Course {
+}
---
+++ javaPatikasi/src/week4practice1/Main.java
@@ -0,0 +1,7 @@
+package week4practice1;
+
+public class w4p1 {
+    public static void main(String[] args) {
+        System.out.println("Student IS");
+    }
+}
