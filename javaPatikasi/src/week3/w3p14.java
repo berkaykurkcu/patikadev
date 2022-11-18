0ef20ed26084bb8536e40cee58690771c19c6a00
berkaykurkcu <berkay.kurkcuyt@gmail.com>
2022-11-18 14:30:30 +0300

    implemented w3p14

--- javaPatikasi/src/week3/w3p14.java
+++ javaPatikasi/src/week3/w3p14.java
@@ -1,2 +1,19 @@
-package week3;public class w3p14 {
+package week3;
+
+import java.util.Scanner;
+
+public class w3p14 {
+    public static void main(String[] args) {
+        System.out.println("Üslü sayı hesaplayan program");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter bottom digit: ");
+        int bottom = scan.nextInt();
+        System.out.println("Enter power: ");
+        int power = scan.nextInt();
+        int result = 1;
+        for(int i = 1; i<= power; i++){
+            result *= bottom;
+        }
+        System.out.println("Result: " +result);
+    }
 }
