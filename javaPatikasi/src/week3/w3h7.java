76ab12315a75a00116a505d630d4c9cf6beb6ff8
berkaykurkcu <berkay.kurkcuyt@gmail.com>
2022-11-18 14:47:24 +0300

    w3h7

--- javaPatikasi/src/week3/w3h7.java
+++ javaPatikasi/src/week3/w3h7.java
@@ -1,2 +1,29 @@
-package week3;public class w3h7 {
+package week3;
+
+import java.util.Scanner;
+
+public class w3h7 {
+    public static void main(String[] args) {
+        System.out.println("Desene göre method oluşturma");
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+        System.out.println("Output: ");
+        nRecursive(nRecursive(number), number);
+        //overloaded functions with different outcomes as expected.-
+    }
+
+    static int nRecursive(int n){
+        if (n <= 0){
+            return n;
+        }
+        System.out.print(n + " ");
+        return nRecursive(n-5);
+    }
+    static int nRecursive(int n, int i){
+        if (n > i){
+            return n;
+        }
+        System.out.print(n + " ");
+        return nRecursive(n+5, i);
+    }
 }
